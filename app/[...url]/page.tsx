0833502af01576da8d7d361fc35f85dfc2922410
Waqar-ahmedkhan <waqarahmed44870@gmail.com>
2024-08-14 11:06:46 +0500

    main

--- app/[...url]/page.tsx
+++ app/[...url]/page.tsx
@@ -1,29 +1,41 @@
-import { ragChat } from "@/lib/Rag-Chat"
+import ChatWrapper from "@/components/ChatWrapper";
+import { ragChat } from "@/lib/Rag-Chat";
+import { redis } from "@/lib/redis";
 
 interface PageProps {
-    params: {
-        url: string | string[] | undefined,
+  params: {
+    url: string | string[] | undefined;
+  };
+}
+
+function reConstractUrl({ url }: { url: string[] }) {
+  const decodedComponent = url.map((component) => {
+    return decodeURIComponent(component);
+  });
+
+  return decodedComponent.join('/');
+}
+
+const page = async ({ params }: PageProps) => {
+  const reconstractedUrl = reConstractUrl({ url: params.url as string[] });
+  try {
+    const isAlreadyIndexed = await redis.sismember("indexed-urls", reconstractedUrl);
+    console.log("isalreadyindexed", isAlreadyIndexed);
+    const sessionId = "mock-session"
+
+    if (!isAlreadyIndexed) {
+      await ragChat.context.add({
+        type: "html",
+        source: reconstractedUrl,
+        config: { chunkOverlap: 50, chunkSize: 200 },
+      });
+      await redis.sadd("indexed-urls", reconstractedUrl);
     }
-}
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
 
-function reConstractUrl({url}:{url: string[]}) {
-  const decodedComponent = url.map((component)=> {
-     decodeURIComponent(component)
-  }) 
+  return <ChatWrapper/>
+};
 
-  return decodedComponent.join('/')
-}
-const page = async ({params}: PageProps) => {
-  const reconstractedUrl = reConstractUrl({url: params.url as string[]})
-  await ragChat.context.add({
-    type: "html",
-    source: reconstractedUrl,
-    config: { chunkOverlap: 50, chunkSize: 200},
-  })
-    console.log(params);
-  return (
-    <div>hello</div>
-  )
-}
-
-export default page
+export default page;

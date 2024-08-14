58276f5478d6b6f700b3595d36cb7d87a9892f7d
Waqar-ahmedkhan <waqarahmed44870@gmail.com>
2024-08-14 09:53:37 +0500

    add

--- app/[...url]/page.tsx
+++ app/[...url]/page.tsx
@@ -1,9 +1,26 @@
+import { ragChat } from "@/lib/Rag-Chat"
+
 interface PageProps {
     params: {
         url: string | string[] | undefined,
     }
 }
-function page({params}: PageProps) {
+
+function reConstractUrl({url}:{url: string[]}) {
+  const decodedComponent = url.map((component)=> {
+     decodeURIComponent(component)
+  }) 
+
+  return decodedComponent.join('/')
+}
+const page = async ({params}: PageProps) => {
+  const reconstractedUrl = reConstractUrl({url: params.url as string[]})
+  await ragChat.context.add({
+    type: "html",
+    source: reconstractedUrl,
+    config: { chunkOverlap: 50, chunkSize: 200},
+  })
+    console.log(params);
   return (
     <div>hello</div>
   )

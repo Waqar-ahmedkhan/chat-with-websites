be6c7ecca5871990d5400abe80b23d13d8d6c995
Waqar-ahmedkhan <waqarahmed44870@gmail.com>
2024-08-14 16:19:50 +0500

    'added'

--- app/[...url]/page.tsx
+++ app/[...url]/page.tsx
@@ -18,11 +18,11 @@ function reConstractUrl({ url }: { url: string[] }) {
 
 const page = async ({ params }: PageProps) => {
   const reconstractedUrl = reConstractUrl({ url: params.url as string[] });
-  try {
+  const sessionId = "mock-session"
+
     const isAlreadyIndexed = await redis.sismember("indexed-urls", reconstractedUrl);
     console.log("isalreadyindexed", isAlreadyIndexed);
-    const sessionId = "mock-session"
-
+    
     if (!isAlreadyIndexed) {
       await ragChat.context.add({
         type: "html",
@@ -31,11 +31,13 @@ const page = async ({ params }: PageProps) => {
       });
       await redis.sadd("indexed-urls", reconstractedUrl);
     }
-  } catch (error) {
-    console.error("An error occurred:", error);
-  }
 
-  return <ChatWrapper/>
-};
+
+   
+  
+
+
+  return <ChatWrapper sessionId={sessionId} />
+}
 
 export default page;
--- components/ChatWrapper.tsx
+++ components/ChatWrapper.tsx
@@ -2,12 +2,26 @@
 
 import { useChat } from "ai/react";
 
-function ChatWrapper({sessionId, initialMassages}: {sessionId: string, initialMassages: string[]}) {
- const {} = useChat({
-  api: "api/chat-stream",
+function ChatWrapper({sessionId}: {sessionId: string}) {
+ const {messages, handleInputChange, input, handleSubmit } = useChat({
+  api: "/api/chat-stream",
+  body: {sessionId}
+
  })
 
-  return <div>ChatWrapper</div>;
+  return <div className="relative min-h-full bg-zinc-900  flex divide-y divide-zinc-700 flex-col justify-between gap-2">
+    <div className="flex-1 text-black bg-zinc-800 justify-between  flex flex-col">
+      {JSON.stringify(messages)}
+    </div>
+
+    
+   
+    <form action="" onSubmit={handleSubmit}>
+    <input type="text" className="text-black" value={input} onChange={handleInputChange} />
+    <button className="text-white bg-blue-500 hover:bg-blue-700 p-2 rounded-md" type="submit">Send</button>
+    </form>
+
+  </div>;
 }
 
 export default ChatWrapper;
